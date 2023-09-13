1aaa4626b565333ea6fe8a6cb13af3c3d8f37d66
hjkim <hjkim@newfire-tech.com>
2023-09-13 10:57:16 +0900

    Remove stroyboard

--- AlarmService/AlarmService/Src/VC/ViewController.swift
+++ AlarmService/AlarmService/Src/VC/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    private let disposeBag = DisposeBag()
     private var datas: [SaveData] = []
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var editButton: UIButton!
-    @IBOutlet weak var deleteButton: UIButton!
+    
+    private let titleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Your alarms"
+        lb.textColor = .purple
+        lb.font = UIFont.boldSystemFont(ofSize: 24)
+        return lb
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.separatorStyle = .none
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.bouncesZoom = false
+        return tableView
+    }()
+    
+    private let addButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Add", for: .normal)
+        btn.update(type: .normal)
+        return btn
+    }()
+    
+    private let editButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Edit", for: .normal)
+        btn.update(type: .normal)
+        return btn
+    }()
+    
+    private let deleteButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Delete", for: .normal)
+        btn.update(type: .normal)
+        return btn
+    }()
     
     deinit {
         NotificationCenter.default.removeObserver(self, name: Constants.RefreshAlarms, object: nil)
@@ -22,10 +58,84 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.register(AlarmItemCell.self, forCellReuseIdentifier: "AlarmItemCell")
+        
+        setupViews()
+        action()
         refresh()
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.refresh), name: Constants.RefreshAlarms, object: nil)
+    }
+    
+    private func setupViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+        
+        let sv = UIStackView(arrangedSubviews: [addButton, editButton, deleteButton])
+        sv.axis = .horizontal
+        sv.spacing = 10.0
+        sv.alignment = .leading
+        sv.distribution = .fill
+        view.addSubview(sv)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(14)
+        }
+        sv.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-40)
+        }
+        tableView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(14)
+            make.bottom.equalTo(sv.snp.top).offset(-20)
+        }
+    }
+    
+    private func action() {
+        addButton.rx.tap.bind { [weak self] in
+            guard let self = self else {
+                return
+            }
+            let customInitData = AddInitData(editMode: false, data: nil)
+            let dataProvider = BaseVCInitDataProvider(customInitData: customInitData)
+            let vc = AddViewController(initDataProvider: dataProvider)
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }.disposed(by: disposeBag)
+        
+        editButton.rx.tap.bind { [weak self] in
+            guard let self = self else {
+                return
+            }
+            let customInitData = AddInitData(editMode: true, data: SaveDataManager.shared.getSelectedData())
+            let dataProvider = BaseVCInitDataProvider(customInitData: customInitData)
+            let vc = AddViewController(initDataProvider: dataProvider)
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }.disposed(by: disposeBag)
+        
+        deleteButton.rx.tap.bind { [weak self] in
+            guard let self = self else {
+                return
+            }
+            if let data = datas.first(where: { $0.option == true }) {
+                PopupVC.showAlert(vc: self, title: "알림", message: "'\(data.desc)' 스케줄에서 삭제하시겠습니까?", okHandler: {
+                    if SaveDataManager.shared.delete(id: data.id) {
+                        PopupVC.showAlert(vc: self, title: "알림", message: "'\(data.desc)' 스케줄에서 삭제했습니다.") {
+                            self.refresh()
+                        }
+                    }
+                }, cancelHandler: {
+                    
+                })
+            } else {
+                PopupVC.showAlert(vc: self, title: "알림", message: "아이템을 먼저 선택해주세요.")
+            }
+        }.disposed(by: disposeBag)
     }
     
     @objc func refresh() {
@@ -39,38 +149,6 @@ class ViewController: UIViewController {
         } else {
             editButton.isEnabled = false
             deleteButton.isEnabled = false
-        }
-    }
-
-    @IBAction func onAdd(_ sender: Any) {
-        let customInitData = AddInitData(editMode: false, data: nil)
-        let dataProvider = BaseVCInitDataProvider(customInitData: customInitData)
-        let vc = AddViewController(initDataProvider: dataProvider)
-        vc.modalPresentationStyle = .overFullScreen
-        self.present(vc, animated: true)
-    }
-    
-    @IBAction func onEdit(_ sender: Any) {
-        let customInitData = AddInitData(editMode: true, data: SaveDataManager.shared.getSelectedData())
-        let dataProvider = BaseVCInitDataProvider(customInitData: customInitData)
-        let vc = AddViewController(initDataProvider: dataProvider)
-        vc.modalPresentationStyle = .overFullScreen
-        self.present(vc, animated: true)
-    }
-    
-    @IBAction func onDelete(_ sender: Any) {
-        if let data = datas.first(where: { $0.option == true }) {
-            PopupVC.showAlert(vc: self, title: "알림", message: "'\(data.desc)' 스케줄에서 삭제하시겠습니까?", okHandler: {
-                if SaveDataManager.shared.delete(id: data.id) {
-                    PopupVC.showAlert(vc: self, title: "알림", message: "'\(data.desc)' 스케줄에서 삭제했습니다.") {
-                        self.refresh()
-                    }
-                }
-            }, cancelHandler: {
-                
-            })
-        } else {
-            PopupVC.showAlert(vc: self, title: "알림", message: "아이템을 먼저 선택해주세요.")
         }
     }
 }
